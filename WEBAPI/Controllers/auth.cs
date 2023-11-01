8ab546f73a3a1ceb6f0264c1ee876447d0c54eba
tolik <tolik@DESKTOP-0JR6UK9>
2023-11-01 16:27:00 +0300

    Add project files.

---
+++ WEBAPI/Controllers/auth.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WEBAPI.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class auth : Controller
+    {
+        [HttpPost]
+        public IActionResult Auth([FromBody] LoginModel loginData)
+        {
+            if (loginData != null && IsValidUser(loginData.Username, loginData.Password))
+            {
+                return Ok("OK");
+            }
+            else
+            {
+                return Unauthorized("OSHIBKA");
+            }
+        }
+        private bool IsValidUser(string username, string password)
+        {
+            return username == "User" && password == "Pass";
+        }
+    }
+}
---
+++ WEBAPI/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace WEBAPI
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
